45bbea23c477572d2d5167b0157f264eee3c301e
deon george <deongeorgevadakkel@gmail.com>
2026-09-05 12:59:38 +0530

    ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
     proxy: {
       '/api': 'http://localhost:5000',
       '/static': 'http://localhost:5000',
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
